5bc37044f4c9f89eb6c4c73d4933a1c8612a1ee4
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-01-28 21:48:11 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -94,7 +94,7 @@ bot.on("message", function(message) {
 
     if (command === "servers"){
     let guilds = bot.guilds.map((guild) => `- ${guild.name} (${guild.id})`);
-    message.channel.send(`I'm in the following guilds: \n${guilds.join}('\n')`);
+    message.channel.send(`I'm in the following guilds:\n${i + 1}.${guilds.join('\n')}`);
     }
 
     if (command === "discrim") {
