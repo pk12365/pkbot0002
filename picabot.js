2cf111d21bfb115b0ae6067ac663c9b804408df4
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-01-29 02:58:34 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -106,7 +106,7 @@ bot.on("message", function(message) {
         var args3 = message.content.substring(12);
         message.channel.send(args3);
         message.channel.send(`guild ${guild}`);
-        guild.leave();
+        //guild.leave();
         message.channel.send('Left guild.');
     }
 
