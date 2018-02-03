b84fb38e0b1a0fbda6748b46cd3ffe9cd881af44
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-02-03 18:37:02 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const cleverbot = require('cleverbot.io');
 const request = require('request');
 const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
@@ -53,6 +54,22 @@ fs.readFile("save.json", function(err, data) {
         } else {
             throw err;
         }
+    }
+});
+
+bot.on("message", message => {
+	if (message.content.startsWith(`<@${bot.user.id}>`)) {
+		message.channel.startTyping();
+		const content = message.content.slice(21);
+		let cbot = new cleverbot(process.env.CBOT_USER, process.env.CBOT_KEY);
+		cbot.setNick(message.author.username);
+		cbot.create(function (err, session) {
+			cbot.ask(content, function (err, response) {
+				message.channel.send(response);
+				message.channel.stopTyping();
+			});
+		});
+		return;
     }
 });
 
