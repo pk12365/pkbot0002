6a3d3f11ac5059085d09d79fa363238d9c8c9866
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-02-22 00:55:25 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -181,7 +181,27 @@ bot.on("message", function(message) {
         });
     }
 
-    if (command === "eval") {
+	if(command === 'eval') {
+		const code = args.join(" ");
+		const token = client.token.split("").join("[^]{0,2}");
+		const rev = client.token.split("").reverse().join("[^]{0,2}");
+		const filter = new RegExp(`${token}|${rev}`, "g");
+		try {
+			let output = eval(code);
+			if (output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
+			output = inspect(output, { depth: 0, maxArrayLength: null });
+			output = output.replace(filter, "[TOKEN]");
+			output = clean(output);
+			if (output.length < 1950) {
+				message.channel.send(`\`\`\`js\n${output}\n\`\`\``);
+			} else {
+				message.channel.send(`${output}`, {split:"\n", code:"js"});
+			}
+		} catch (error) {
+			message.channel.send(`The following error occured \`\`\`js\n${error}\`\`\``);
+		}
+
+    /*if (command === "eval") {
         if(message.author.id !== botowner) {
             message.reply('this command is only for bot owner!!!');
             return;
@@ -205,7 +225,7 @@ bot.on("message", function(message) {
 	    } catch (error) {
 		    message.channel.send(`The following error occured \`\`\`js\n${error}\`\`\``)
 	    }
-    }
+    }*/
 
 /*    if (command === "leaveserver") {
         if(message.author.id !== botowner) {
