98434ef1aff702b1f08d85beae62d42c939000b0
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-02-22 01:31:12 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -181,27 +181,8 @@ bot.on("message", function(message) {
         });
     }
 
-	if(command === 'eval') {
-		const code = args.join(" ");
-		const token = bot.token.split("").join("[^]{0,2}");
-		const rev = bot.token.split("").reverse().join("[^]{0,2}");
-		const filter = new RegExp(`${token}|${rev}`, "g");
-		try {
-			let output = eval(code);
-			if (output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
-			output = inspect(output, { depth: 0, maxArrayLength: null });
-			output = output.replace(filter, "[TOKEN]");
-			output = clean(output);
-			if (output.length < 1950) {
-				message.channel.send(`\`\`\`js\n${output}\n\`\`\``);
-			} else {
-				message.channel.send(`${output}`, {split:"\n", code:"js"});
-			}
-		} catch (error) {
-			message.channel.send(`The following error occured \`\`\`js\n${error}\`\`\``);
-		}
 
-    /*if (command === "eval") {
+    if (command === "eval") {
         if(message.author.id !== botowner) {
             message.reply('this command is only for bot owner!!!');
             return;
@@ -213,7 +194,7 @@ bot.on("message", function(message) {
 	    const filter = new RegExp(`${token}|${rev}`, "g");
 	    try {
 		    let output = eval(code);
-		    if (output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
+		    //if (output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
 		    output = inspect(output, { depth: 0, maxArrayLength: null });
 		    output = output.replace(filter, "[TOKEN]");
 		    output = clean(output);
@@ -225,7 +206,7 @@ bot.on("message", function(message) {
 	    } catch (error) {
 		    message.channel.send(`The following error occured \`\`\`js\n${error}\`\`\``)
 	    }
-    }*/
+    }
 
 /*    if (command === "leaveserver") {
         if(message.author.id !== botowner) {
