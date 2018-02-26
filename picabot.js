2edc999e9216fb1d4b9a3f47480d2a6bfcb61667
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-02-26 13:36:28 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -299,6 +299,7 @@ bot.on("message", function(message) {
         let kickUser = message.mentions.users.first();
         let reason = args.slice(1).join(" ");
         if (!kickUser) return message.channel.send(`Specify a user to kick`);
+        message.channel.send(reason);
         if (!reason) return message.channel.send("You did not give a reason to kick the user.")
         if(!kickUser.id == message.author.id) return message.channel.send("You cannot kick yourself!");
         if (message.author.highestRole.position < kickUser.user.highestRole.position) return message.channel.send("Your role is either the same or lower than the user you wish to kick.");
