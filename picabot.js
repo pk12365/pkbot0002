716b35d2056cda359aa98c710633c2aba407dc79
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-02-27 18:47:46 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -70,8 +70,9 @@ bot.on("message", async(message) => {
     const command = args.shift().toLowerCase();
 
     if(command == "gsearch" || command === "google" || command === "g") {
+        let args3 = message.content.substring(command.length);
         let searchMessage = await message.reply('Searching... Sec.');
-        let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(message.content)}`;
+        let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(args3)}`;
         message.channel.send(`${searchUrl}`);
         return snekfetch.get(searchUrl).then((result) => {
             let $ = cheerio.load(result.text);
