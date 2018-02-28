3ac02a6711a20e8294fa44b2bfa13fc4b5498780
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-02-28 13:00:12 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -158,10 +158,10 @@ bot.on("message", function(message) {
             .setColor(randomcolor)
             .setAuthor("Hi " + message.author.username.toString(), message.author.avatarURL)
             .setDescription(`ICW help Section \nPrefix = ${prefix} \nvolume command is for all users \nmore commands coming soon`)
-            .addField("Bot info commands", `invite - (bot invite link)\nbotinfo - (info about the bot) \nuptime - (uptime of the bot)\nservers - (bots servers)`)
-            .addField("until commands", `cleverbot - (talk with bot with mention or icw \`\`example - icw hi\`\`) \ngoogle - (search anything) \nweather - (check your city weather) \nsay - (bot saying your message) \ndiscrim - (found any discriminators) \nserverinfo - (info about server)`)
+            .addField("Bot info commands", `invite - (bot invite link)\nbotinfo - (info about the bot)\`\`info , botstatus\`\` \nuptime - (uptime of the bot)\nservers - (bots servers)`)
+            .addField("until commands", `cleverbot - (talk with bot with mention or icw \`\`example - icw hi\`\`) \`\`icw\`\` \ngoogle - (search anything) \`\`gsearch , g , \`\` \nweather - (check your city weather) \nsay - (bot saying your message) \ndiscrim - (found any discriminators) \nserverinfo - (info about server)`)
             .addField("Modration command", `warn - (for warning a member) \n kick - (for kick a member) \n ban - (for ban a member)`)
-            .addField("Music commands", `play - (for serach and add your song in thre queue) \npause - (pause the player) \nresume - (resume the player) \nvolume - (set your player volume) \nskip - (for next song) \nprev - (for previos song) \nstop - (for stop the player) \nqueue - (for check playlist) \nsong - (view current song) \nrandom - (playing randomly)`)
+            .addField("Music commands", `play - (for serach and add your song in thre queue) \`\`p\`\` \npause - (pause the player) \nresume - (resume the player) \nvolume - (set your player volume) \`\`sv , setvolume\`\` \nskip - (for next song) \`\`s , next\`\` \nprev - (for previos song) \nstop - (for stop the player) \nqueue - (for check playlist) \`\`q , playlist\`\` \nsong - (view current song) \`\`np , nowplaying\`\` \nrandom - (playing randomly)`)
             .setThumbnail("https://media.discordapp.net/attachments/406099961730564107/407455733689483265/Untitled6.png?width=300&height=300")
             .setFooter("Bot Developed by: PK#1650 ", "https://cdn.discordapp.com/attachments/399064303170224131/405585474988802058/videotogif_2018.01.24_10.14.40.gif")
             .addField("support server", `[link](https://discord.gg/zFDvBay)`, inline = true)
@@ -268,7 +268,7 @@ bot.on("message", function(message) {
         message.channel.send("please check your dms", { replay: message }).then(sent => sent.delete({ timeout: 99 }));
     }
 
-    if (command === "botinfo" || command === "info") {
+    if (command === "botinfo" || command === "info" || command === "botstatus") {
         let TextChannels = bot.channels.filter(e => e.type !== 'voice').size;
         let VoiceChannels = bot.channels.filter(e => e.type === 'voice').size;
         var infoembed = new Discord.RichEmbed()
@@ -403,7 +403,7 @@ bot.on("message", function(message) {
     /*------------------------------------------------------------------------------------------
                                             MUSIC COMMANDS
     -------------------------------------------------------------------------------------------*/
-    if (command === "play" || command === "p" || command === "m p") {
+    if (command === "play" || command === "p") {
         if (message.member.voiceChannel !== undefined) {
             if (args.length > 0) {
                 //var query = "";
@@ -500,7 +500,7 @@ bot.on("message", function(message) {
     }
 
 
-    if (command === "skip" || command === "next") {
+    if (command === "skip" || command === "next" || command === "s") {
         if (message.member.voiceChannel !== undefined) {
             if (!message.guild.me.voiceChannel) {
                 message.channel.send("bot is not in voice channel and nothing to play", { reply: message });
@@ -624,7 +624,7 @@ bot.on("message", function(message) {
         }
     }
 
-    if (command === "song" || command === "np") {
+    if (command === "song" || command === "np" || command === "nowplaying") {
         if (!message.guild.me.voiceChannel) {
             message.channel.send("bot is not in voice channel and nothing to play", { reply: message });
             return;
@@ -643,7 +643,7 @@ bot.on("message", function(message) {
         }
     }
 
-    if (command === "queue" || command === "q") {
+    if (command === "queue" || command === "q" || command === "playlist") {
         if (!message.guild.me.voiceChannel) {
             message.channel.send("bot is not in voice channel and nothing to play", { reply: message });
             return;
