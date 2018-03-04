79b1261cdf165aea0def449c29864c1742cdc90d
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-03-04 21:30:09 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -31,13 +31,13 @@ const snekfetch = require('snekfetch');
 const querystring = require('querystring');
 const firebase = require("firebase");
 
-var app = firebase.initializeApp({
-      'apiKey': process.env.FB_API_KEY,
-      'authDomain': process.env.FB_AUTH_DOMAIN,
-      'databaseURL': process.env.FB_DATABASE_URL,
-      'projectId': process.env.FB_PROJECT_ID,
-      'storageBucket': process.env.FB_STORAGE_BUCKET,
-      'messagingSenderId': process.env.FB_MESSAGING_SENDER_ID
+firebase.initializeApp({
+      apiKey: process.env.FB_API_KEY,
+      authDomain: process.env.FB_AUTH_DOMAIN,
+      databaseURL: process.env.FB_DATABASE_URL,
+      projectId: process.env.FB_PROJECT_ID,
+      storageBucket: process.env.FB_STORAGE_BUCKET,
+      messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
       });
 var database = firebase.database();
 //firebase.auth().signInWithEmailAndPassword(process.env.FB_EMAIL, process.env.FB_PASSWORD);
