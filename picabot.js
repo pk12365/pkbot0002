c8b61cd2f895bb272d81dd321b5a3dde584973d1
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-03-11 23:33:52 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -155,7 +155,7 @@ bot.on("message", function(message) {
             //.ref(`/servers/${message.guild.id}/`)
             //.once('value',(snapshot) => {
 
-    if (!message.content.startsWith(prefix)// && !message.content.startsWith((`${snapshot.val().guildprefix}`))) return undefined;
+    if (!message.content.startsWith(prefix)/* && !message.content.startsWith((`${snapshot.val().guildprefix}`)))*/ return undefined;
     //if (message.content.startsWith(prefix)) {
         const randomcolor = '0x' + Math.floor(Math.random() * 16777215).toString(16);
             const args = message.content.substring(prefix.length + 1).split();
