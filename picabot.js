49d9911ccfebb4a18673ade7be677989e3ebff95
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-03-11 19:20:14 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -151,13 +151,10 @@ bot.on('message', message => {
 bot.on("message", function(message) {
     bot.user.setPresence({ status: `streaming`, game: { name: `${prefix}help | ${bot.users.size} Users`, type: `STREAMING`, url: `https://www.twitch.tv/pardeepsingh12365` } });
     if (message.author.bot) return undefined;
-      const guildprefix = firebase.database()
-      .ref(`/servers/${message.guild.id}/`)
-      .once('value',(snapshot) => {(`${snapshot.val().guildprefix}`)
-                                   })
-      guildprefix.some(p => {
+      firebase.database()
+      .ref(`/servers/${message.guild.id}/`).once('value',(snapshot) => {
 
-    if (!message.content.startsWith(prefix) && !message.content.startsWith(p)) return undefined;
+    if (!message.content.startsWith(prefix) && !message.content.startsWith((`${snapshot.val().guildprefix}`))) return undefined;
     });
     const randomcolor = '0x' + Math.floor(Math.random() * 16777215).toString(16);
 
