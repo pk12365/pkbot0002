884ac3244c142ab032fc7298429805cdb55c8d89
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-03-15 21:37:02 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -152,17 +152,17 @@ bot.on("message", function(message) {
     bot.user.setPresence({ status: `streaming`, game: { name: `${prefix}help | ${bot.users.size} Users`, type: `STREAMING`, url: `https://www.twitch.tv/pardeepsingh12365` } });
     if (message.author.bot) return undefined;
       const randomcolor = '0x' + Math.floor(Math.random() * 16777215).toString(16);
-      //firebase.database()
-            //.ref(`/servers/${message.guild.id}/`)
-            //.once('value',(snapshot) => {
+      firebase.database()
+            .ref(`/servers/${message.guild.id}/`)
+            .once('value',(snapshot) => {
 
     if (!message.content.startsWith(prefix) && !message.content.startsWith(prefix2)) return undefined;
     if (message.content.startsWith(prefix)) {
             args = message.content.substring(prefix.length + 4).split();
             comarg = message.content.slice(prefix.length).trim().split(/ +/g);
     } else {
-          args = message.content.substring(prefix2.length + 4).split();
-          comarg = message.content.slice(prefix2.length).trim().split(/ +/g);
+          args = message.content.substring((`${snapshot.val().guildprefix}`).length + 4).split();
+          comarg = message.content.slice((`${snapshot.val().guildprefix}`).length).trim().split(/ +/g);
       //const args = message.content.substring((`${snapshot.val().guildprefix}`).length + 1).split();
     }
             
@@ -769,7 +769,7 @@ bot.on("message", function(message) {
             message.channel.send("you cant change volume if you are not in voice channel", { reply: message });
         }
     }
-//})
+})
 });
 
 var addSong = function(message, url) {
