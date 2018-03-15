0a049e22d4aa4f3bc838da108526e7024dfcff91
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-03-15 20:55:52 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -166,7 +166,7 @@ bot.on("message", function(message) {
       //const args = message.content.substring((`${snapshot.val().guildprefix}`).length + 1).split();
     }
             
-    const command = comar.shift().toLowerCase();
+    const command = comarg.shift().toLowerCase();
     //const command = message.content.toLowerCase().split(" ")[0];
     //command = command.slice(prefix.length);
     if (command === "prefix") {
