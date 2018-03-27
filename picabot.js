f6dc7fad48ebc0ebdc4b94678f176de7b6c59472
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-03-27 10:35:19 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -159,9 +159,8 @@ bot.on("message", function(message) {
     const randomcolor = '0x' + Math.floor(Math.random() * 16777215).toString(16);
 
     let configRef = firebase.database().ref(`/servers/${message.guild.id}/guildprefix/`);
-    let gprefix = configRef.on("value", ss => {
-        value = ss.val();
-    });
+    configRef.on("value", ss => {value = ss.val();})
+    let gprefix = value;
 
     message.channel.send(gprefix)
 
