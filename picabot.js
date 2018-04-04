11cbb879814c9142bfd87c6b770d28afeb20a0f7
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-04-04 15:16:08 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -457,6 +457,61 @@ bot.on("message", async(message) => {
     /*------------------------------------------------------------------------------------------
                                             MUSIC COMMANDS
     -------------------------------------------------------------------------------------------*/
+
+    if (command === "yt") {
+        const youtube = new YouTube("AIzaSyCaOtxOYSSw_zRbR0cUPQS3flCbHS2UlGE");
+        const voiceChannel = message.member.voiceChannel;
+
+        let searchString = args.slice(0).join(" ");
+        const url = args[0] ? args[0].replace(/<(.+)>/g, '$1') : '';
+
+        if (!voiceChannel) return message.channel.send("You are not in a voice channel please join a channel and use this command again");
+
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has('CONNECT')) return message.channel.send("I do not have the permissions to join that voice channel pleae give me permissions to join");
+        if (!permissions.has("SPEAK")) return message.channel.send("I do not have the permissions to speak in that voice channel pleae give me permissions to join");
+
+        if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+          const playlist = await youtube.getPlaylist(url);
+          const videos = await playlist.getVideos();
+          for (const video of Object.values(videos)) {
+            const video2 = await youtube.getVideoByID(video.id);
+            await index.handleVideo(video2, message, voiceChannel, true);
+          }
+          return message.channel.send(`✅ Playlist: **${playlist.title}** has been added to the queue!`);
+        } else {
+          try {
+            var video = await youtube.getVideo(url);
+          } catch (error) {
+            try {
+              var videos = await youtube.searchVideos(searchString, 10);
+              let index = 0;
+              message.channel.send(`
+      __**Song selection:**__
+      ${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
+      Please provide a value to select one of the search results ranging from 1-10.
+                          `);
+              try {
+                var response = await message.channel.awaitMessages(message2 => message2.content > 0 && message2.content < 11, {
+                  maxMatches: 1,
+                  time: 10000,
+                  errors: ['time']
+                });
+              } catch (err) {
+                console.error(err);
+                return message.channel.send('No or invalid value entered, cancelling video selection.');
+              }
+              const videoIndex = parseInt(response.first().content);
+              var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+            } catch (err) {
+              console.error(err);
+              return message.channel.send('🆘 I could not obtain any search results.');
+            }
+          }
+          return index.handleVideo(video, message, voiceChannel);
+        }
+    }
+
     if (command === "play" || command === "p") {
         if (message.member.voiceChannel !== undefined) {
             if (args.length > 0) {
