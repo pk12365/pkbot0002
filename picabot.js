a6b5838c0ae29749e0a5981f1f97b2788478443c
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-04-06 22:11:01 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -243,7 +243,7 @@ bot.on("message", async(message) => {
     }
 
     if (command == "gsearch" || command === "google" || command === "g") {
-        let args3 = message.content.substring(command.length + prefix.length);
+        let args3 = message.content.substring(command.length + prefix.length + 1);
         let searchMessage = await message.reply('Searching... Sec.');
         let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(args3)}`;
         message.channel.send(`'${searchUrl}'`)
