6e35eed3af71a5a365a01a11297b823c00b3f168
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-04-15 09:59:05 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-module.exports = function (lul) {
-  bot.on("message", async(message) => {
-    if (message.content.startsWith("test")) {
-      message.channel.send("gg")
-    }
-  })
+module.exports = {
+  lul: function () {
+    bot.on("message", async(message) => {
+      if (message.content.startsWith("test")) {
+        message.channel.send("gg")
+      }
+    })
+  }
 }
