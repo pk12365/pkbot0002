a369f0fab8075a0a1301e175ef81a0782888af2e
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-04-17 15:34:20 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -725,7 +725,7 @@ bot.on("message", async(message) => {
                         Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(function(font) {
                             image2.print(font, 400, 80, s)
                             Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(function(font) {
-                                image2.print(font, 325, 20, tag);
+                                image2.print(font, 315, 20, tag);
                                 image2.print(font, 330 , 80, m);
                                 image2.print(font, 330 , 135, u);
                                 image.resize(300, 300);
@@ -1312,7 +1312,7 @@ bot.on('guildMemberAdd', async(member) => {
             Jimp.read(img).then(function(image) {
                 Jimp.read(images).then(function(image2) {
                     Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function(font) {
-                        image2.print(font, 325 , 220, fact2);
+                        image2.print(font, 315 , 220, fact2);
                         Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(function(font) {
                             image2.print(font, 400, 80, s)
                             Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(function(font) {
