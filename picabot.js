3a1d7a4647ff745fd23748401330053106e444ec
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-04-29 12:01:19 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -1239,7 +1239,7 @@ var addSong = function(message, video, voiceChannel, playlist = false) {
             playSong(message, connection);
         }).catch(err => bot.channels.get(boterrorchannel).send(`${message.author.username} from ${message.guild.name} play command and error in addsong \n${err}`)); //removed consol log
     }
-    if (dispatcher.paused === true && message.guild.me.voiceChannel) {
+    if (!dispatcher || dispatcher.paused === true && message.guild.me.voiceChannel) {
         message.member.voiceChannel(function(connection) {
         playSong(message, connection); })
     }
